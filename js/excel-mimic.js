c5b997a23a7dcfe21dfb4d9d8f76c388ffb33b02
Seth Corker <seth.corker@gmail.com>
2016-05-07 15:14:43 +1200

    Basic CSS added

--- js/excel-mimic.js
+++ js/excel-mimic.js
@@ -3,6 +3,8 @@ var $table  = $('#xl-table');
 var $header = $('#xl-header');
 var $body   = $('#xl-body');
 
+var tableStore = {};
+
 function letter(num){
   var alpha = "A".charCodeAt(0);
   return String.fromCharCode(alpha + num - 1);
@@ -27,15 +29,18 @@ function indexToLetter(index){
   return result;
 }
 
-//Draw a single table header cell
+//Generate a single table header cell
 function drawHeaderCell(h, index){
-  h.append('<th>' + indexToLetter(index + 1) + '</th>');
+  return '<th>' + indexToLetter(index + 1) + '</th>';
 }
 //Create the table header
 function initGridHeader(h, columns){
+  headList = [];
+  headList.push('<th id="xl-row-index"></th>'); //Empty column header for row index
   for(col = 0; col < columns; col++){
-    drawHeaderCell(h, col);
+    headList.push(drawHeaderCell(h, col));
   }
+  h.append(headList.join(""));
 }
 
 //Draw a single body cell
@@ -48,6 +53,7 @@ function initGridBody(b, columns, rows){
   bodyList = [];
   for(row = 0; row < columns; row++){
     bodyList.push('<tr>');
+    bodyList.push('<td>' + (row + 1) + '</td>'); //Index for row
     for(col = 0; col < rows; col++){
       bodyList.push(drawBodyCell(b, col, row));
     }
@@ -61,6 +67,10 @@ function initGrid(columns, rows){
   initGridHeader($header, columns);
   initGridBody($body, columns, rows);
 }
+
+$body.keypress(function(){
+  console.log('KEY');
+});
 
 $(document).ready(function() {
   console.log("INIT");
